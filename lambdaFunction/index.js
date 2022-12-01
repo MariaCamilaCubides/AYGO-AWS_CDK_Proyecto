75dacaba987f2e6731bc721c7665c57774143ba4
Maria Camila Cubides Martin <macacumar97@gmail.com>
2022-12-01 07:07:46 -0500

    Code for lambda

--- lambdaFunction/index.js
+++ lambdaFunction/index.js
@@ -3,53 +3,46 @@ const pg = require('pg');
 
 exports.handler = async (event, context, callback) => {
     const response = {
-        statusCode: 200,
+        statusCode: 500,
         body: JSON.stringify({
             Error: 'Unexpected error'
         })
     };
     try {
-        const password = 'wSqu4wW6VXR07WbgH5w6^LIevXxuyV'
-        console.log('1')
         const client = new pg.Client({
           user: 'postgres',
-          host: 'finalproyectdb.cpagihdbpowx.us-east-1.rds.amazonaws.com',
-          database: 'finalProyectDb',
-          password,
+          host: `${process.env.DB_ENDPOINT_ADDRESS}`,
+          database: `${process.env.DB_NAME}`,
+          password: `${process.env.DB_PASSWORD}`,
           port: 5432,
         });
-        console.log('2');
         await client.connect();
-        console.log('3')
         const queryText = 'INSERT INTO "timeOff" (id, profile, "hoursAmount","dateTimeRange", "fileUrl", status) VALUES($1, $2, $3, $4, $5, $6)';
         const queryValues = [
             AWS.util.uuid.v4(),
             AWS.util.uuid.v4(),
-            3,
-            [],
+            parseInt(event.amountHours),
+            [new Date(event.initialDateTime), new Date(event.finalDateTime)],
             event.fileUrl,
-            0
+            parseInt(event.status)
         ];
         const queryString = {
             text: queryText,
             values: queryValues,
         };
-        console.log(queryString)
-        const res = await client.query(queryString);
-        console.log(res);
         await client.end()
-        console.log('4')
+        response.statusCode = 200;
+        response.body = 'Time off requested. Waiting for the approval';
     } catch (err) {
-        console.log('Error while trying to connect to db', err);
-        response.body = 'Error'
+        console.log('error', err);
+        response.body = 'Error requesting time off'
     }
     return {
-    statusCode: 200,
-    body: 'hola',
-    headers: {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "OPTIONS,POST"
-    },
+        ...response,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST"
+        },
     };
 };
